1bc5dafb514274face41cd254775e0d4fd732146
alessandrostein <alessandro@alessandrostein.com>
2014-08-11 09:17:35 -0300

    Create packpage and Create class Produto (nome, descricao, unidadeMedida, volume, quantidade, preco)

---
+++ src/br/com/megabrew/model/Produto.java
@@ -0,0 +1,75 @@
+package br.com.megabrew.model;
+
+public class Produto {
+
+	private String nome;
+	private String descricao;
+	private String unidadeDeMedida;
+	private String volume;
+	private int quantidade;
+	private Float preco;
+
+	public Produto() {
+		super();
+	}
+
+	public Produto(String nome, String descricao, String unidadeDeMedida,
+			String volume, int quantidade, Float preco) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.unidadeDeMedida = unidadeDeMedida;
+		this.volume = volume;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUnidadeDeMedida() {
+		return unidadeDeMedida;
+	}
+
+	public void setUnidadeDeMedida(String unidadeDeMedida) {
+		this.unidadeDeMedida = unidadeDeMedida;
+	}
+
+	public String getVolume() {
+		return volume;
+	}
+
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+}
